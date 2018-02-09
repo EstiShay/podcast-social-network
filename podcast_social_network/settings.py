de3e516566a03f4cecd7fa041e454b37cf041bb5
Jeff Bailey <northwestjeff@gmail.com>
2018-02-09 14:20:42 -0800

    update db in settings.py

--- podcast_social_network/settings.py
+++ podcast_social_network/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'podcast_social_network.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {'default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_BROWN_URL"])}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
