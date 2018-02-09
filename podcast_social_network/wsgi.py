2bef44b6429684298b724ca1f02866c420734aff
Jeff Bailey <northwestjeff@gmail.com>
2018-02-09 13:08:11 -0800

    updated DATABASES in settings.py and added dj static in wsgi.py

--- podcast_social_network/wsgi.py
+++ podcast_social_network/wsgi.py
@@ -11,11 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "podcast_social_network.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
 
 # import os
 # from django.core.wsgi import get_wsgi_application
