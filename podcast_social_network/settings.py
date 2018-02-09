4ef4baf75559b922be2e2aea1aebf682e0ec898c
Jeff Bailey <northwestjeff@gmail.com>
2018-02-09 11:51:40 -0800

    message

--- podcast_social_network/settings.py
+++ podcast_social_network/settings.py
@@ -134,5 +134,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
-
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
